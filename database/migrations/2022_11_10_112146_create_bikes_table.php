1a6c095674477342af6ed93c368d0ac1a4a0dd46
svalvastic <assogames2@gmail.com>
2022-11-10 12:29:00 +0100

    ✨ feat(db bikes): add migration bike

---
+++ database/migrations/2022_11_10_112146_create_bikes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bikes', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 16);
+            $table->string('model', 32);
+            $table->boolean('change')->default(false);
+            $table->string('type', 32);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bikes');
+    }
+}
